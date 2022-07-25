733b7a5a76f4dc5351d956ec5eb10be206960408
Felipe Gontijo Alpino <51760899+felipealpino@users.noreply.github.com>
2022-07-25 12:49:48 -0300

    webpack extra config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 require('dotenv').config();
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
-console.log(process.env.NODE_ENV);
+console.log('NODE_ENV => ', process.env.NODE_ENV);
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
@@ -32,7 +32,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(j|t)sx$/,
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -44,7 +44,13 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          { loader: 'style-loader' }, // to inject the result into the DOM as a style block
+          // { loader: 'css-modules-typescript-loader' }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+          { loader: 'css-loader', options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+          { loader: 'sass-loader' }, // to convert SASS to CSS
+          // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
+        ],
       },
     ],
   },
